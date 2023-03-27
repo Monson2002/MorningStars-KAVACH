b3b0aa5058e1303e6d36b93439e5eef6da86c532
monson2002 <monsonreji2002@gmail.com>
2023-03-27 19:39:41 +0530

    first commit

--- objectDetection.py
+++ objectDetection.py
@@ -15,14 +15,13 @@ plt.imshow(cv2.cvtColor(edged, cv2.COLOR_BGR2RGB))
 keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(keypoints)
 contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
-
 location = None
 for contour in contours:
     approx = cv2.approxPolyDP(contour, 10, True)
     if len(approx) == 4:
         location = approx
         break
-
+    
 mask = np.zeros(gray.shape, np.uint8)
 new_image = cv2.drawContours(mask, [location], 0,255, -1)
 new_image = cv2.bitwise_and(img, img, mask=mask)
