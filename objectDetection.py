10a50a8855a34616cb98e61bb8e215846313c258
monson2002 <monsonreji2002@gmail.com>
2023-03-28 21:51:58 +0530

    28-3-23 9:51pm Added more images

--- objectDetection.py
+++ objectDetection.py
@@ -4,7 +4,7 @@ import numpy as np
 import imutils
 import easyocr
 
-img = cv2.imread('image4.jpg')
+img = cv2.imread('image5.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(cv2.cvtColor(gray, cv2.COLOR_BGR2RGB))
 
